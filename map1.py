73537fb9fdbe8806f675522109d76ce71c077f49
mousahmed <mous.ahmed@outlook.com>
2022-03-27 15:03:12 +0200

    added layer control

--- map1.py
+++ map1.py
@@ -2,7 +2,7 @@ import folium
 import pandas
 
 map = folium.Map(location=[38.5, -99.09], zoom_start=5, tiles="Stamen Terrain")
-fg = folium.FeatureGroup(name="My map")
+fg = folium.FeatureGroup(name="Volcaneos")
 data = pandas.read_csv("Volcanoes.txt")
 lon = list(data["LON"])
 lat = list(data["LAT"])
@@ -35,10 +35,15 @@ for lt, ln, el, name in zip(lat, lon, elev, name):
         color="grey",
         fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+fp = folium.FeatureGroup(name="Population")
+
+fp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
                             style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000
                                                       else 'orange' if 20000000 > x['properties']['POP2005'] >= 10000000
                                                       else 'red'}))
 
 map.add_child(fg)
+map.add_child(fp)
+map.add_child(folium.LayerControl())
+
 map.save("map1.html")
